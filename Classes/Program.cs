0932033b2c56c5897985da16936191e49b0dc7b9
Bryseida Hernandez <bryseida.hernandez@yahoo.com>
2025-01-06 21:40:08 -0800

    completed exercise 1

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var carOne = new Car();
+            carOne.Make = "Jeepy";
+            carOne.Model = "Wrangler";
+            carOne.Year = 2011;
+            
+            Console.WriteLine($"This is a {carOne.Make} {carOne.Model}, made in the year {carOne.Year}.");
+            
         }
     }
 }
